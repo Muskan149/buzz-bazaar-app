1cd946282535b2a62b1f32eab762bee90853fbb3
Muskan Mahajan <muskanmahajan2004@gmail.com>
2024-03-21 21:18:00 -0400

    changed

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -5,7 +5,8 @@ import { supabase } from '../js/supabaseConfig.js'
 const fileTypes = ["JPEG", "JPG", "PNG", "GIF"];
 
 const ImageUpload = ({ onPublicURLChange }) => {
-  const [file, setFile] = useState(null);
+  const [URL, setURL] = useState(null);
+  const [label, setLabel] = useState("Upload or Drop Image here: ");
   
   const handleChange = async (file) => {
     const { data, error } = await supabase.storage
@@ -24,17 +25,36 @@ const ImageUpload = ({ onPublicURLChange }) => {
     .storage
     .from("images")
     .getPublicUrl(`./${file.name}`)
-    
+    setURL(publicURL.publicUrl)
     console.log(publicURL.publicUrl)
     onPublicURLChange(publicURL.publicUrl); // Pass publicURL back to parent component
-}
+    setLabel("Uploaded successfully!")
+
+  }
+
+  const handleDeleteClick = () => {
+    setURL(null);
+    setLabel("Upload or Drop Image here: ")
+  };
 
   return (
+    
+  <>
+  <div className="image-upload-section">
     <FileUploader
-    handleChange={(file) => handleChange(file)}
-    name="image"
-    types={fileTypes}
-  />
+      handleChange={(file) => handleChange(file)}
+      name="image"
+      label={label}
+      hoverTitle="Drop here"
+      types={fileTypes}
+      children={<div><p>{label}</p></div>}
+    />
+    {URL && (<button class ="img-delete-button" onClick={handleDeleteClick}>Remove Image</button>)}
+  </div>
+  
+  {URL && <img className="img-preview" src={URL} alt="Uploaded" />}
+  
+  </>
   );
   }
 
